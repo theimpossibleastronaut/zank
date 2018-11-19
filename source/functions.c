theimpossibleastronaut/zank

commit 78fabc667c40f6f36ac630da2406075bcd9c3016

restructure map, allow more than one object to be in a cell
This temporarily breaks the function for receiving map positions from
clients.

source/functions.c
@@ -30,7 +30,8 @@
 bool
 accuse (st_player_data *player)
 {
-  if (map[player->pos_x][player->pos_y] == indicted_politician)
+  extern st_map map;
+  if (map.cell[player->cell].object[0] == indicted_politician)
   {
     printw ("This politician has been indicted recently.\n");
     return 0;
@@ -63,12 +64,12 @@ accuse (st_player_data *player)
     player->health = player->health - 5;
   }
 
-  map[player->pos_x][player->pos_y] = indicted_politician;
+  map.cell[player->cell].object[0] = indicted_politician;
   return 1;
 }
 
 void
-showitems (objects * object)
+showitems (st_objects * object)
 {
 
   printw ("Your inventory:\n");
@@ -154,35 +155,37 @@ lake (void)
 }
 
 void
-showMap (st_player_data *player, bool Visited[X][Y])
+showMap (st_player_data *player)
 {
+  extern st_map map;
   int column, row;
-
   short print_row = 2;
-
+  int ctr = 0;
   for (column = X - 1; column >= 0; column--)
   {
     mvprintw (print_row++, 64, "");
 
     for (row = 0; row < Y; row++)
     {
-      if (column == player->pos_x && row == player->pos_y)
+      if (ctr == player->cell)
         printw ("@");
-      else if (Visited[column][row] == 1)
+      else if (map.cell[ctr].is_explored == 1)
       {
-        if (map[column][row] == politician)
+        if (map.cell[ctr].object[0] == politician)
           printw ("p");
-        else if (map[column][row] == Wall)
+        else if (map.cell[ctr].object[0] == Wall)
           printw ("W");
-        else if (map[column][row] == Lake)
+        else if (map.cell[ctr].object[0] == Lake)
           printw ("L");
-        else if (map[column][row] == Grapevine)
+        else if (map.cell[ctr].object[0] == Grapevine)
           printw ("G");
         else
           printw ("X");
       }
       else
         printw ("*");
+
+      ctr++;
     }
   }
 }
@@ -190,9 +193,10 @@ showMap (st_player_data *player, bool Visited[X][Y])
 void
 prompt (st_player_data *player, short pCtr, short iCtr)
 {
+  extern st_map map;
   mvprintw (22, 0,
             "\n(%d,%d) (politicians left to retire: %d) (HP: %d) (i,m,q[uit]) (e,w,n,s)? ",
-            player->pos_y, player->pos_x, pCtr - iCtr, player->health);
+            map.cell[player->cell].pos_y, map.cell[player->cell].pos_x, pCtr - iCtr, player->health);
 }
 
 bool
@@ -210,24 +214,15 @@ void change_pos(st_player_data *player, const char c, int which)
   extern const int sfd;
   extern const bool is_client;
   char buf[1024];
-  switch (c)
-  {
-    case 'y':
-      player->pos_y = player->pos_y + direction[which].offset;
-      break;
-    case 'x':
-      player->pos_x = player->pos_x + direction[which].offset;
-      break;
-    default:
-      break;
-  }
+  player->cell = player->cell + direction[which].offset;
 
   if (is_client)
   {
     char y_str_pos[4];
     char x_str_pos[4];
-    itoa (player->pos_y, y_str_pos);
-    itoa (player->pos_x, x_str_pos);
+    extern st_map map;
+    itoa (map.cell[player->cell].pos_y, y_str_pos);
+    itoa (map.cell[player->cell].pos_x, x_str_pos);
     int len = snprintf (buf, BUF_SIZE, "pos = %s,%s", y_str_pos, x_str_pos);
     len++;
     if (write (sfd, buf, len) != len)

source/net.c
@@ -143,14 +143,15 @@ run_server (void)
       char *chomp_ptr;
       if (strncmp (buf, "pos", 3) == 0)
       {
-        chomp_ptr = strtok (buf, "=");
+        /*chomp_ptr = strtok (buf, "=");
         chomp_ptr = strtok (NULL, ",");
         del_char_shift_left (' ', &chomp_ptr);
-        clients[cl_num]->pos_y = atoi (chomp_ptr);
+        clients[cl_num].cell->pos_y = atoi (chomp_ptr);
         chomp_ptr = strtok (NULL, ",");
         clients[cl_num]->pos_x = atoi (chomp_ptr);
 
         printf ("position for %s is %d,%d\n", host, clients[cl_num]->pos_y, clients[cl_num]->pos_x);
+        */
       }
 
       printf ("Received %zd bytes from %s:%s\n\n", nread, host, service);

source/Zank.h
@@ -48,16 +48,38 @@
  *  Define the grid size
  */
 
-#define X 10
-#define Y 10
+#ifndef X
+  #define X 10
+#endif
+#ifndef Y
+  #define Y 10
+#endif
+
+/*
+ * if parentheses aren't used here, rand doesn't produce
+ * good results due to operator precedence
+ */
+#define MAP_SIZE (X * Y)
 
 #define BUF_SIZE 512 + 1
+#define MAX_CELL_OBJECTS 4
 
 #define PACKAGE "Zank"
 #define VERSION "0.0.6-dev"
 #define AUTHOR "Andy Alt"
 
-int map[X][Y];
+typedef struct st_cell st_cell;
+struct st_cell {
+  int pos_x;
+  int pos_y;
+  int object[MAX_CELL_OBJECTS];
+  bool is_explored;
+};
+
+typedef struct st_map st_map;
+struct st_map {
+  st_cell cell[MAP_SIZE];
+};
 
 enum {
   WEST,
@@ -76,12 +98,11 @@ typedef struct st_player_data st_player_data;
 struct st_player_data {
   char address[BUF_SIZE];
   int health;
-  int pos_x;
-  int pos_y;
+  int cell;
 };
 
-typedef struct objects objects;
-struct objects {
+typedef struct st_objects st_objects;
+struct st_objects {
   int documents;
   int rings;
   int swords;

source/functions.h
@@ -27,11 +27,11 @@
 #include "Zank.h"
 
 bool accuse (st_player_data *player);
-void showitems (objects * object);
+void showitems (st_objects * object);
 void tree (void);
 void wall (void);
 void lake (void);
-void showMap (st_player_data *player, bool Visited[X][Y]);
+void showMap (st_player_data *player);
 void prompt (st_player_data *player, short pCtr, short iCtr);
 bool borderPatrol (st_player_data *player);
 void change_pos(st_player_data *player, const char c, int which);

source/main.c
@@ -29,11 +29,13 @@
 #include "usage.h"
 #include "net.h"
 
+st_map map;
+
 st_direction direction[] = {
   { "West", -1 },
   { "East", 1 },
-  { "North", 1 },
-  { "South", -1 }
+  { "North", -Y },
+  { "South", Y }
 };
 
 /*
@@ -98,8 +100,7 @@ main (int argc, char* const *argv)
 /**
  * Starting position
  */
-  player.pos_x = X / 2;
-  player.pos_y = Y / 2;
+  player.cell = MAP_SIZE / 2;
 
   if (is_server)
     run_server ();
@@ -159,57 +160,59 @@ main (int argc, char* const *argv)
 
   bool flag = 0;
 
-  srand (time (0));
+  srand (time (NULL));
 
   int randgrape;
 
 /**
  * Generate coordinates for Magic Waterfall
  */
-  int rx, ry;
-  rx = rand () % X;
-  ry = rand () % Y;
+  int waterfall_pos = rand () % MAP_SIZE;
+  map.cell[waterfall_pos].object[0] = MagicWaterfall;
 
-  const unsigned short politician_total = X * Y / 6;
-
-/**
+/*
+ *
  * Initialize map
+ *
  */
-  unsigned short column, row;
-  int politicianCtr = 0;
 
-  map[rx][ry] = MagicWaterfall;
+  int pos_x, pos_y;
+  int num = 0;
+  for (pos_x = 0; pos_x < X; pos_x++)
+    for (pos_y = 0; pos_y < Y; pos_y++)
+    {
+      map.cell[num].pos_x = pos_x;
+      map.cell[num].pos_y = pos_y;
+      num++;
+    }
+
+  const unsigned short politician_total = MAP_SIZE / 6;
+  int politicianCtr = 0;
 
   while (politicianCtr < politician_total)
   {
-    int rand_x, rand_y;
-    rand_x = rand () % X;
-    rand_y = rand () % Y;
-
-    if (map[rand_x][rand_y] != politician &&
-        (map[rand_x][rand_y] != MagicWaterfall))
+    int politician_pos = rand () % MAP_SIZE;
+    if (map.cell[politician_pos].object[0] != politician &&
+        (map.cell[politician_pos].object[0] != MagicWaterfall))
     {
-      map[rand_x][rand_y] = politician;
+      map.cell[politician_pos].object[0] = politician;
       politicianCtr++;
     }
   }
 
-  objects object;
+  st_objects object = { 0,0,0,0,0};
 
-  for (column = 0; column < X; column++)
+  int ctr;
+  for (ctr = 0; ctr < MAP_SIZE; ctr++)
   {
-    for (row = 0; row < Y; row++)
+    do
     {
-      do
-      {
-        object.pos = rand () % 8;
-      }
-      while (object.pos == politician);
+      object.pos = rand () % 8;
+    }while (object.pos == politician);
 
-      if (map[column][row] != politician &&
-          map[column][row] != MagicWaterfall)
-        map[column][row] = object.pos;
-    }
+    if (map.cell[ctr].object[0] != politician &&
+        map.cell[ctr].object[0] != MagicWaterfall)
+      map.cell[ctr].object[0] = object.pos;
   }
 
   politicianCtr = politician_total;
@@ -222,19 +225,6 @@ main (int argc, char* const *argv)
   printw ("By %s\n", AUTHOR);
   printw ("With code contributions from mzdelong\n");
   refresh ();
-
-  /* prompt(politicianCtr, indictedCtr);
-     refresh(); */
-
-  bool Visited[X][Y];
-  /**
-   * Init Array
-   */
-  for (column = 0; column < X; column++) {
-    for (row = 0; row < Y; row++) {
-      Visited[column][row] = 0;
-    }
-  }
 
   bool isStarting = 1;
 
@@ -291,28 +281,28 @@ main (int argc, char* const *argv)
     switch (c)
     {
     case 'e':
-      if (player.pos_y < Y - 1)
+      if (map.cell[player.cell].pos_y < Y -1)
         change_pos(&player, 'y', EAST);
       else
         flag = borderPatrol (&player);
       break;
 
     case 'w':
-      if (player.pos_y > 0)
+      if (map.cell[player.cell].pos_y > 0)
         change_pos(&player, 'y', WEST);
       else
         flag = borderPatrol (&player);
       break;
 
     case 's':
-      if (player.pos_x > 0)
+      if (map.cell[player.cell].pos_x < X - 1)
         change_pos (&player, 'x', SOUTH);
       else
         flag = borderPatrol (&player);
       break;
 
     case 'n':
-      if (player.pos_x < X - 1)
+      if (map.cell[player.cell].pos_x > 0)
         change_pos (&player, 'x', NORTH);
       else
         flag = borderPatrol (&player);
@@ -346,9 +336,9 @@ main (int argc, char* const *argv)
 
     if (!flag)
     {
-      Visited[player.pos_x][player.pos_y] = 1;
+      map.cell[player.cell].is_explored = 1;
 
-      object.pos = map[player.pos_x][player.pos_y];
+      object.pos = map.cell[player.cell].object[0];
 
       printw ("\nYou see %s\n", mapObject[object.pos]);
 
@@ -376,8 +366,8 @@ main (int argc, char* const *argv)
 
       if (acquire)
       {
-        printw ("Acquiring %s\n", mapObject[map[player.pos_x][player.pos_y]]);
-        map[player.pos_x][player.pos_y] = Seed;
+        printw ("Acquiring %s\n", mapObject[map.cell[player.cell].object[0]]);
+        map.cell[player.cell].object[0] = Seed;
       }
 
       printw ("\n");
@@ -449,7 +439,7 @@ main (int argc, char* const *argv)
         printw
           ("You swim at the base of the Magic Waterfall and feel much better. (+40 hp)\n");
         player.health = player.health + 40;
-        map[player.pos_x][player.pos_y] = Dried_up_Waterfall;
+        map.cell[player.cell].object[0] = Dried_up_Waterfall;
         break;
 
       case Clearing:
@@ -466,10 +456,10 @@ main (int argc, char* const *argv)
            * map[x][player.pos_y] will equal one of the items for the enum declaration
            * item 5-7
            */
-            map[player.pos_x][player.pos_y] = t + an_incriminating_document;
+            map.cell[player.cell].object[0] = t + an_incriminating_document;
           }
           else
-            map[player.pos_x][player.pos_y] = Grapevine;
+            map.cell[player.cell].object[0] = Grapevine;
         }
 
         t = rand () % 4;
@@ -519,7 +509,7 @@ main (int argc, char* const *argv)
       case Seed:
         printw ("Acquiring Seed.\n");
         object.seeds++;
-        map[player.pos_x][player.pos_y] = Clearing;
+        map.cell[player.cell].object[0] = Clearing;
         break;
 
       case Grapevine:
@@ -543,7 +533,7 @@ main (int argc, char* const *argv)
       }
     }
     prompt (&player, politicianCtr, indictedCtr);
-    showMap (&player, Visited);
+    showMap (&player);
     refresh ();
   }
 

commit 4522d6191ba690c9ece191a7180975e116385ec4

fix parsing received client positions
that I broke in
https://github.com/theimpossibleastronaut/Zank/commit/78fabc667c40f6f36ac630da2406075bcd9c3016

source/functions.c
@@ -218,12 +218,10 @@ void change_pos(st_player_data *player, const char c, int which)
 
   if (is_client)
   {
-    char y_str_pos[4];
-    char x_str_pos[4];
+    char cell_num[BUF_SIZE];
     extern st_map map;
-    itoa (map.cell[player->cell].pos_y, y_str_pos);
-    itoa (map.cell[player->cell].pos_x, x_str_pos);
-    int len = snprintf (buf, BUF_SIZE, "pos = %s,%s", y_str_pos, x_str_pos);
+    itoa (player->cell, cell_num);
+    int len = snprintf (buf, BUF_SIZE, "cell = %s", cell_num);
     len++;
     if (write (sfd, buf, len) != len)
     {

source/net.c
@@ -140,17 +140,15 @@ run_server (void)
       printf ("incoming string from %s: %s\n", host, buf);
 
       char *chomp_ptr;
-      if (strncmp (buf, "pos", 3) == 0)
+      if (strncmp (buf, "cell", 4) == 0)
       {
-        /*chomp_ptr = strtok (buf, "=");
-        chomp_ptr = strtok (NULL, ",");
-        del_char_shift_left (' ', &chomp_ptr);
-        clients[cl_num].cell->pos_y = atoi (chomp_ptr);
-        chomp_ptr = strtok (NULL, ",");
-        clients[cl_num]->pos_x = atoi (chomp_ptr);
-
-        printf ("position for %s is %d,%d\n", host, clients[cl_num]->pos_y, clients[cl_num]->pos_x);
-        */
+        chomp_ptr = strtok (buf, "=");
+        clients[cl_num]->cell = atoi (chomp_ptr);
+        extern const st_map map;
+        printf ("position for %s is %d,%d\n",
+                host,
+                map.cell[clients[cl_num]->cell].pos_y,
+                map.cell[clients[cl_num]->cell].pos_x);
       }
 
       printf ("Received %zd bytes from %s:%s\n\n", nread, host, service);